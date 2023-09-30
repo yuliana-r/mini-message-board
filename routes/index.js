2e3c80bbebbd4ba99277e2605e8c246f6513571d
yuli-r <iuliana.rotaru234@gmail.com>
2023-09-30 21:17:11 +0100

    Style index page

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,28 @@ var router = express.Router();
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Yuli",
+    text: "I'm reading a book on anti-gravity. It's impossible to put down!",
+    user: "PunMaster123",
     added: new Date()
   },
   {
-    text: "Meowy meow meow",
-    user: "Charlie the cat",
+    text: "Why did the cat sit on the computer? Because it wanted to keep an eye on the mouse! 😺🖱️",
+    user: "PurrfectHacker",
+    added: new Date()
+  },
+  {
+    text: "I told my wife she was drawing her eyebrows too high. She looked surprised!",
+    user: "BrowArtist",
+    added: new Date()
+  },
+  {
+    text: "Why don't scientists trust atoms? Because they make up everything!",
+    user: "AtomWhisperer",
+    added: new Date()
+  },
+  {
+    text: "I'm on a seafood diet. I see food and I eat it... especially if it's fish! 🐟😸 Bon appétit, meow!",
+    user: "MrFishBoy",
     added: new Date()
   }
 ]
