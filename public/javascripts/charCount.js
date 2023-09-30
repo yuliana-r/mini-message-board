679d790741908f35450448adc58b81c0af8577ab
yuli-r <iuliana.rotaru234@gmail.com>
2023-09-30 20:16:39 +0100

    Add form page

---
+++ public/javascripts/charCount.js
@@ -0,0 +1,22 @@
+const textarea = document.getElementById("message");
+const charCount = document.getElementById("charCount");
+
+const maxCount = 250;
+
+console.log("Script loaded");
+
+textarea.addEventListener("input", function updateCharCount() {
+    const currCount = textarea.value.length;
+    charCount.textContent = currCount;
+
+    if (currCount >= maxCount) {
+        charCount.style.color = "#E83A47";
+        return;
+    } else {
+        charCount.style.color = "#F2BA1D";
+    }
+})
+
+module.exports = {
+    updateCharCount
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Yuli",
+    added: new Date()
+  },
+  {
+    text: "Meowy meow meow",
+    user: "Charlie the cat",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'tiny talkies' });
+  res.render('index', { title: 'tiny talkies', messages: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'tiny talkies'});
+})
+
+router.post('/new', function(req, res, next) {
+  console.log("request body is", req.body);
+
+  const messageText = req.body.message;
+  const messageUser = req.body.name;
+  console.log(messageText);
+  console.log(messageUser);
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+  console.log(messages);
+  res.redirect('/');
+})
 
 module.exports = router;
