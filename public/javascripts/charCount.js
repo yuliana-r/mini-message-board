85bfc0b7642ef20501ab940499b8639d58403125
Yuliana R <iuliana.rotaru234@gmail.com>
2025-05-25 16:21:40 +0100

    Enable opening message details in new page

--- public/javascripts/charCount.js
+++ public/javascripts/charCount.js
@@ -1,20 +1,22 @@
-const textarea = document.getElementById("message");
-const charCount = document.getElementById("charCount");
+const textarea = document.getElementById('message');
+const charCount = document.getElementById('charCount');
 
 const maxCount = 150;
 
-textarea.addEventListener("input", function updateCharCount() {
-    const currCount = textarea.value.length;
-    charCount.textContent = currCount;
+textarea.addEventListener('input', function updateCharCount() {
+  const currCount = textarea.value.length;
+  charCount.textContent = currCount;
 
-    if (currCount >= maxCount) {
-        charCount.style.color = "#E83A47";
-        return;
-    } else {
-        charCount.style.color = "#F2BA1D";
-    }
-})
+  if (currCount >= maxCount) {
+    charCount.style.color = '#E83A47';
+    return;
+  } else {
+    charCount.style.color = '#F2BA1D';
+  }
+});
 
-module.exports = {
-    updateCharCount
+if (typeof module === 'object') {
+  module.exports = {
+    updateCharCount,
+  };
 }
--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,50 @@ var router = express.Router();
 
 const messages = [
   {
-    text: "WE WERE ON A BREAK!!!",
-    user: "Ross",
-    added: new Date(Date.parse("02/13/1997"))
+    id: 0,
+    text: 'WE WERE ON A BREAK!!!',
+    user: 'Ross',
+    added: new Date(Date.parse('02/13/1997')),
   },
   {
+    id: 1,
     text: "What matters isn't if people are good or bad. What matters is, if they're trying to be better today than they were yesterday.",
-    user: "Michael, TGP",
-    added: new Date(Date.parse("04/23/2023"))
+    user: 'Michael, TGP',
+    added: new Date(Date.parse('04/23/2023')),
   },
   {
+    id: 2,
     text: "It gets easier. Every day it gets a little easier. But you gotta do it every day. That's the hard part.",
-    user: "BoJack H.",
-    added: new Date(Date.parse("10/01/2023"))
-  }
-]
+    user: 'BoJack H.',
+    added: new Date(Date.parse('10/01/2023')),
+  },
+];
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'tiny talkies', messages: messages });
 });
 
-router.get('/new', function(req, res, next) {
-  res.render('form', { title: 'tiny talkies'});
-})
+router.get('/messages/:messageId', function (req, res, next) {
+  const { messageId } = req.params;
+  res.send(
+    `Message id: ${messageId}; message: ${JSON.stringify(messages[messageId])}`,
+  );
+});
 
-router.post('/new', function(req, res, next) {
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'tiny talkies' });
+});
+
+router.post('/new', function (req, res, next) {
   const messageText = req.body.message;
   const messageUser = req.body.name;
-  messages.push({text: messageText, user: messageUser, added: new Date()});
+  messages.push({
+    id: messages.length,
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
   res.redirect('/');
-})
+});
 
 module.exports = router;
