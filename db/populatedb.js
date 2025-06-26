fbbbab035b9fd37929cdb6c7486e61418fdebc2e
Yuliana R <iuliana.rotaru234@gmail.com>
2025-06-26 21:58:22 +0100

    Edit populatedb script

--- db/populatedb.js
+++ db/populatedb.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 
 const SQL = `
 CREATE TABLE IF NOT EXISTS messages (
-  id INTEGER PRIMARY KEY,
-  text TEXT NOT NULL,
-  user VARCHAR(255) NOT NULL,
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  message TEXT NOT NULL,
+  author VARCHAR ( 255 ) NOT NULL,
   added DATE NOT NULL
 );
 
-INSERT INTO messages (id, text, user, added)
+INSERT INTO messages (message, author, added)
 VALUES
-  (0, 'WE WERE ON A BREAK!!!', 'Ross', '1997-02-13'),
-  (1, 'What matters isn''t if people are good or bad. What matters is, if they''re trying to be better today than they were yesterday.', 'Michael, TGP', '2023-04-23'),
-  (2, 'It gets easier. Every day it gets a little easier. But you gotta do it every day. That''s the hard part.', 'BoJack H.', '2023-10-01');
+  ('WE WERE ON A BREAK!!!', 'Ross', '1997-02-13'),
+  ('What matters isn''t if people are good or bad. What matters is, if they''re trying to be better today than they were yesterday.', 'Michael, TGP', '2023-04-23'),
+  ('It gets easier. Every day it gets a little easier. But you gotta do it every day. That''s the hard part.', 'BoJack H.', '2023-10-01');
 `;
 
 async function main() {
