98d6d481fbd0df3ada49543ef0a1817a7df55238
Yuliana R <iuliana.rotaru234@gmail.com>
2025-06-26 22:16:15 +0100

    Add static files command

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 
 app.listen(port, () => {
