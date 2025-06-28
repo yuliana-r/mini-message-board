cf950f98482a6487ec47391e1872abcaea889bc7
Yuliana R <iuliana.rotaru234@gmail.com>
2025-06-28 15:43:21 +0100

    Tidy up & add enable development mode to run code locally

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const connectionString = isDevelopment
+  ? process.env.DATABASE_PUBLIC_URL
+  : process.env.DATABASE_URL;
 
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: connectionString,
 });
